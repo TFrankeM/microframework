a3c2de2ebee54db9587ea0526578d15d6d41b2bf
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2024-04-21 18:21:56 -0300

    Implemente parada de TimerTrigger

--- Framework/Trigger/Trigger.cpp
+++ Framework/Trigger/Trigger.cpp
@@ -1,43 +1,61 @@
 #include "Trigger.h"
 
 // Constructor for the abstract Trigger class.
-Trigger::Trigger(Queue* queue) 
-    : outputQueue(queue) 
-    {}
+Trigger::Trigger(Queue *queue, std::function<void()> callback)
+    : outputQueue(queue), callback(callback)
+{
+}
 
 // Destructor for the Trigger class. Ensures the trigger thread is properly joined.
-Trigger::~Trigger() {
-    if (triggerThread.joinable()) 
+Trigger::~Trigger()
+{
+    if (triggerThread.joinable())
         triggerThread.join();
 }
 
 // Starts the trigger thread to run asynchronously.
-void Trigger::start() {
+void Trigger::start()
+{
     triggerThread = std::thread(&Trigger::fire, this);
 }
 
 // Constructor for TimerTrigger. Initializes the interval and callback.
-TimerTrigger::TimerTrigger(Queue* queue, long interval, std::function<void()> callback)
-    : Trigger(queue)
-    , interval(interval)
-    , callback(callback) 
-    {}
+TimerTrigger::TimerTrigger(Queue *queue, std::function<void()> callback, long interval)
+    : Trigger(queue, callback), interval(interval)
+{
+}
 
 // Implementation of the fire method for TimerTrigger. Repeatedly calls the callback at the set interval.
-void TimerTrigger::fire() {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+void TimerTrigger::fire()
+{
+    while (active)
+    {
         callback();
+        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
     }
 }
 
-RequestTrigger::RequestTrigger(Queue* queue) 
-    : Trigger(queue) 
-    {}
-
-void RequestTrigger::fire() {
+// Overrides the start method to set the active flag before starting the trigger thread.
+void TimerTrigger::start()
+{
+    active = true;
+    Trigger::start();
 }
 
-void RequestTrigger::waitForRequest() {
-    fire();
+// Stops the timer trigger by joining the trigger thread.
+void TimerTrigger::stop()
+{
+    active = false;
+    if (triggerThread.joinable())
+        triggerThread.join();
+}
+
+RequestTrigger::RequestTrigger(Queue *queue, std::function<void()> callback)
+    : Trigger(queue, callback)
+{
+}
+
+void RequestTrigger::fire()
+{
+    callback();
 }
--- Framework/Trigger/Trigger.h
+++ Framework/Trigger/Trigger.h
@@ -8,53 +8,58 @@
 
 /**
  * @brief The Trigger class serves as an abstract base for various trigger mechanisms.
- * 
+ *
  * It manages a thread that executes a triggering action asynchronously.
  */
-class Trigger {
+class Trigger
+{
 protected:
     // Queue to hold DataFrames for processing
-    Queue* outputQueue;
+    Queue *outputQueue;
     // Thread to handle the asynchronous triggering
     std::thread triggerThread;
-
-public:
-    explicit Trigger(Queue* queue);
-    virtual ~Trigger();
-    virtual void fire() = 0; // Pure virtual function to be implemented by derived classes
-    void start(); // Starts the trigger thread
-};
-
-/**
- * @brief The TimerTrigger class invokes a callback function at regular intervals.
- * 
- * It's useful for periodic tasks like polling or scheduled data processing.
- */
-class TimerTrigger : public Trigger {
-private:
-    // Interval in milliseconds between each trigger
-    long interval;
     // Callback function to be called on trigger
     std::function<void()> callback;
 
 public:
-    TimerTrigger(Queue* queue, long interval, std::function<void()> callback);
+    explicit Trigger(Queue *queue, std::function<void()> callback);
+    virtual ~Trigger();
+    virtual void fire() = 0; // Pure virtual function to be implemented by derived classes
+    virtual void start();    // Starts the trigger thread
+};
+
+/**
+ * @brief The TimerTrigger class invokes a callback function at regular intervals.
+ *
+ * It's useful for periodic tasks like polling or scheduled data processing.
+ */
+class TimerTrigger : public Trigger
+{
+private:
+    // Interval in milliseconds between each trigger
+    long interval;
+    // Flag to indicate if the timer trigger is active
+    bool active = false;
+
+public:
+    TimerTrigger(Queue *queue, std::function<void()> callback, long interval);
     // Implementation of the fire method for timer-based triggering
     void fire() override;
+    void start() override; // Overrides the start method to set the active flag
+    void stop();           // Stops the timer trigger
 };
 
 /**
  * @brief The RequestTrigger class is designed to be triggered by external events or requests.
- * 
+ *
  * For example, it could be used to trigger processing when a network request is received.
  */
-class RequestTrigger : public Trigger {
+class RequestTrigger : public Trigger
+{
 public:
-    RequestTrigger(Queue* queue);
+    RequestTrigger(Queue *queue, std::function<void()> callback);
     // Implementation of the fire method for request-based triggering
     void fire() override;
-    // Method to simulate or wait for an external request
-    void waitForRequest();
 };
 
 #endif // TRIGGER_H
